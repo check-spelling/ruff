c4e4e1b932398e2f72ff592329cac9becb1613a2
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-09-11 21:02:31 -0400

    spelling: the
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- crates/ruff_python_parser/src/parser.rs
+++ crates/ruff_python_parser/src/parser.rs
@@ -165,7 +165,7 @@ pub fn parse(source: &str, mode: Mode, source_path: &str) -> Result<ast::Mod, Pa
 
 /// Parse the given Python source code using the specified [`Mode`] and [`TextSize`].
 ///
-/// This function allows to specify the location of the the source code, other than
+/// This function allows to specify the location of the source code, other than
 /// that, it behaves exactly like [`parse`].
 ///
 /// # Example
